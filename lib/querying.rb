be554c866d530bea3df274532de2381d14c2c032
Emily S <emily.sugano@gmail.com>
2018-06-05 21:30:08 +0000

    automatically backed up by learn

--- lib/querying.rb
+++ lib/querying.rb
@@ -42,8 +42,8 @@ end
 
 def select_character_names_and_number_of_books_they_are_in
   "SELECT characters.name
-   FROM character_books
-   INNER JOIN characters
+   FROM characters
+   INNER JOIN character_books
    ON characters.id = character_books.character_id
    GROUP BY character_books.book_id
    ORDER BY characters.name DESC;
